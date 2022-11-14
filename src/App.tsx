cee0d181c4d47f80b6979a6978939247c78dab89
lbhasemeyer <lbhasemeyer@gmail.com>
2022-11-13 20:24:48 -0700

    moving fetch into App to prevent re-fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+// import React from 'react';
 import AnimalDetails from './components/AnimalDetails';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import { Routes, Route } from 'react-router-dom';
 
 function App(){
+  const [allAnimals, setAllAnimals] = useState<any[]>([]);
+  const [getAnimalsError, setGetAnimalsError] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect');
+    const animalsUrl = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
+    fetch(animalsUrl, {
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setAllAnimals(data);
+      
+    })
+    .catch(error => setGetAnimalsError('Something went wrong herding the animals - please refresh page to try again.'));
+  }, [])
+
+  console.log('allAnimals: ', allAnimals);
+  console.log('getAnimalsError: ', getAnimalsError);
   
   return (
     <React.Fragment>
       <Nav />
       <Routes>
-        <Route path='/animals' element={<Home />} />
+        <Route path='/animals' element={<Home animals={allAnimals} animalsError={getAnimalsError} />} />
         {/* <Route path='/animals/:id' element={<AnimalDetails} /> */}
         {/* <Route path='*' element={<Error404 />} */}
       </Routes>
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,40 +8,19 @@ interface animalProps {
   id: number
 }
 
-function Home() {
-  const [allAnimals, setAllAnimals] = useState<any[]>([]);
-  const [getAnimalsError, setGetAnimalsError] = useState('');
-
-  useEffect(() => {
-    const animalsUrl = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
-    fetch(animalsUrl, {
-      mode: 'cors',
-      headers: {
-        'Access-Control-Allow-Origin':'*'
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      setAllAnimals(data);
-      
-    })
-    .catch(error => setGetAnimalsError('Something went wrong herding the animals - please refresh page to try again.'));
-  }, [])
-
-console.log('all animals', allAnimals)
+function Home(props:any) {
+console.log('props: ', props) 
 
   const animalData = 
-    allAnimals.map((animal: animalProps) => {
-      return <OneAnimal name={animal?.name} active_time={animal?.active_time} habitat={animal?.habitat} id={animal?.id} />
+    props.animals.map((animal: animalProps) => {
+      return <OneAnimal key={animal?.id} name={animal?.name} active_time={animal?.active_time} habitat={animal?.habitat} id={animal?.id} />
     });
-
-  console.log('animalData: ', animalData)
 
   return (
     <div>
       <>
         <h1>All animals</h1>
-        {getAnimalsError !== '' ? getAnimalsError : null}
+        {props.animalsError !== '' ? props.animalsError : null}
         {animalData}
       </>
     </div>
